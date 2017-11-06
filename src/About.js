a4803a557c30daa44b9dc0e701b90e09d3e7ebac
Grant Custer <grantcuster@gmail.com>
2017-11-06 15:32:32 -0500

    add files

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Provider, Heading, Box, Text, Flex, colors } from "rebass";
+
+class About extends Component {
+  render() {
+    return <Box>About</Box>;
+  }
+}
+
+export default About;
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Provider, Heading, Box, Text, Flex, colors } from "rebass";
+
+class Home extends Component {
+  render() {
+    return <Box>Home</Box>;
+  }
+}
+
+export default Home;
